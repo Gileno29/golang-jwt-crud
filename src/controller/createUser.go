9fe49513c5fdbe1f8c00e661445ab2a844f22ddf
Gileno Duarte <gileno.cr.duarte@gmail.com>
2024-11-25 12:18:13 -0300

    feat: implementaca a rota de criaçao de usuarios

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,20 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/Gileno29/golang-jwt-crud/src/configuration/rest_err"
+	"github.com/Gileno29/golang-jwt-crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Rota invocada de forma errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s\n", err))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
