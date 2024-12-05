674f93259c2e8dc4de8a9d6e4e2824ab3e38cb47
Gileno <gileno.cr.duarte@gmail.com>
2024-12-05 17:46:47 -0300

    iniciando as views

--- src/controller/user_controller.go
+++ src/controller/user_controller.go
@@ -6,7 +6,9 @@ import (
 )
 
 func NewUserControllerInterface(serviceInterface service.UserDomainService) UserControllerInterface {
-	return &userControllerInterface{}
+	return &userControllerInterface{
+		service: serviceInterface,
+	}
 }
 
 type UserControllerInterface interface {
